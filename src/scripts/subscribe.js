182f9e9cf71dfc752e50a2a5ded66d22f01cd260
firdovsi <megatron_mega@mail.ru>
2024-08-01 19:57:11 +0400

    fix

---
+++ src/scripts/subscribe.js
@@ -0,0 +1,28 @@
+document
+  .getElementById("subscribe")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    if (email) {
+      alert("Thank you for subscribing, " + email + "!");
+      console.log("Thank you for subscribing, " + email + "!");
+      fetch("subscribe.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({ email: email }),
+      })
+        .then((response) => response.text())
+        .then((result) => {
+          alert(result);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      alert("Please fill out both fields.");
+    }
+  });
